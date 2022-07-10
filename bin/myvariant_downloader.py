d545260f4704be049fdd59e5806eee8de95ce159
Nicktf <49584439+1511878618@users.noreply.github.com>
2022-07-10 20:23:22 +0800

    更新myvariant_downloader.py

--- bin/myvariant_downloader.py
+++ bin/myvariant_downloader.py
@@ -1,4 +1,3 @@
-#!/d/anaconda/python
 #!/usr/bin/env python3
 
 import json
@@ -12,7 +11,7 @@ import httplib2
 import pandas as pd
 
 
-def download_ChrRange(chr="chr1", start=69000, end=70000, email=None, as_dataframe=False, GeneSymbol="unknown"):
+def download_ChrRange(chr="chr1", start=69000, end=70000, as_dataframe=False, GeneSymbol="unknown", **kwargs):
     """
     download_ChrRange 从https://myvariant.info/v1/api#/整合数据库中获取指定基因组区域内所有SNP
 
@@ -20,12 +19,14 @@ def download_ChrRange(chr="chr1", start=69000, end=70000, email=None, as_datafra
         chr (str, optional): 输入染色体号. Defaults to "chr1". e.g. chr1 chrX chrY ...
         start (int, optional): 区域的起始位置. Defaults to 69000.
         end (int, optional): 区域的终止位置. Defaults to 70000.
-        email (str, None): 邮箱地址，以免不小心被myvariant数据库当爬虫杀掉。。 
+         
         as_dataframe (bool, optional): 是否输出dataframe. Defaults to False.
         GeneSymbol: GeneSymbol
+        kwargs: fetch_all=True 下载所有的数据; email: email (str, None): 邮箱地址，以免不小心被myvariant数据库当爬虫杀掉。。 ; fields 指定过滤的fields;参见https://docs.myvariant.info/en/latest/doc/data.html#data-sources
     Returns:
         _type_: list or dataframe
     """
+
     # 构建http请求
     h = httplib2.Http()
     headers = {'content-type': 'application/x-www-form-urlencoded'}
@@ -33,53 +34,64 @@ def download_ChrRange(chr="chr1", start=69000, end=70000, email=None, as_datafra
     hits = []  # 检索到的snp存放
     scroll_id = None  # 继续请求后续的SNP的key， see more:https://docs.myvariant.info/en/latest/doc/variant_query_service.html#query-syntax
     gotsnps = 0  # 已经获取到的Snp个数，如果小于检索回来的total则说明该区域的snp数量超过1000个，而超过1000个则需要使用scroll_id来继续请求，这样的机制是反爬虫的一种~。。。。
-    total = 10  #随便一个初始值保证循环开始即可
+    total = 1  #随便一个初始值保证循环开始即可
 
     while gotsnps < total:  # 确保下载完该区域所有的snp 
-        params = f"q={chr}:{start}-{end}&fetch_all=True"
+        params = f"q={chr}:{start}-{end}" 
+
+        params +=''.join([f"&{k}={v}" for k,v in kwargs.items()])  # 添加额外的query内容
+
         if scroll_id:
             params += f"&scroll_id={scroll_id}" 
         else:
             pass 
 
-        if email:  #添加email 以方便myvariants追踪我们在干什么，避免不小心被当爬虫干掉了哈哈哈哈哈
-            params += f"&email={email}"
-        else:
-            pass 
-
-        # print(params)
         res, con = h.request('http://myvariant.info/v1/query', 'GET', params, headers=headers)
         con = json.loads(con)
-        hits.append(con["hits"])  # 添加
+        hits.append(con["hits"])  # 添加下载到的数据到列表中保存
 
         total = con["total"]  # 更新total
         gotsnps += len(con["hits"])  # 更新计数
-        scroll_id = con["_scroll_id"]  # 更新scroll_id
-        # print(f"chr{chr}:{start}-{end} contains {total} snp and already download {gotsnps}")
-    print(f"finish query from https://myvariant.info/v1/api#/ \n got {total} snp at this genomic location:{GeneSymbol} and already download {gotsnps}")
+
+        if total == 0:  #如果这个区域没有则终止循环
+            break 
+
+        try:  #如果不是fetch_all则没有它
+            scroll_id = con["_scroll_id"]  # 更新scroll_id
+        except:  #则下载完了可以结束
+            break 
+
+    print(f"finish query from https://myvariant.info/v1/api#/ \n got {total} snp at this genomic location:{GeneSymbol} and already download {gotsnps}\n")
+
+    hits = list(chain(*hits))  # 一定要加上list，否则会少一行？一个chain的bug?
 
     if as_dataframe:  # 输出df
-        return pd.json_normalize(chain(*hits))
+        return pd.json_normalize(hits)
     else:
-        return chain(*hits)
+        return hits
 
 
-def downloadAndSaveByBed(data, save_path, email):
+def downloadAndSaveByBed(data, save_path, **kwargs):
     """
     downloadAndSaveByBed 通过myvariant数据库，根据Bed文件下载对应的区域内所有的SNP数据
 
     Args:
         data (dataframe): bed的数据，读入后
         save_path (str): 保存目录，按bed文件第四列命名
-        email (str): 邮箱
+        kwargs: fetch_all=True 下载所有的数据; email: email (str, None): 邮箱地址，以免不小心被myvariant数据库当爬虫杀掉。。 ; fields 指定过滤的fields;参见https://docs.myvariant.info/en/latest/doc/data.html#data-sources
     """
-    # os.mkdir(dir)
+    try:    
+        os.mkdir(save_path)
+    except:
+        pass
+
     error = []
+    print(f"文件下载保存在:{save_path}")
     for idx, values in data.iterrows():
         genename = values["GeneSymbol"]
         try:
             output = f"{save_path}/{genename}.csv"
-            out = download_ChrRange(as_dataframe=True, email=email, **values.to_dict())  # 获取染色体号、起始位置、终止位置作为输入
+            out = download_ChrRange(as_dataframe=True,fetch_all=True, **values.to_dict(), **kwargs)  # 获取染色体号、起始位置、终止位置作为输入
             out.to_csv(output, index=None)
         except:
             error.append(genename)
@@ -87,7 +99,7 @@ def downloadAndSaveByBed(data, save_path, email):
         time.sleep(3)
 
 
-def downloadByThread(dataframe, email, threads_num=10, save_path="./associateGeneRangeSNP"):
+def downloadByThread(dataframe, threads_num=10, save_path="./associateGeneRangeSNP",*args, **kwargs):
 
     """
     downloadByThread 多线程的对myvariant数据库进行请求下载，输入bed文件格式的dataframe
@@ -112,15 +124,13 @@ def downloadByThread(dataframe, email, threads_num=10, save_path="./associateGen
         tmp_data = dataframe.iloc[i:i+steps, :]
         thread = threading.Thread(
         target=downloadAndSaveByBed, 
-        args=(tmp_data, save_path, email)
+        args=(tmp_data, save_path),
+        kwargs=kwargs
         )
         threads.append(thread)
     
     for thread in threads:
-        print(f"threading is on ")
-        thread.setDaemon(True)
         thread.start()
-        thread.join()  # 加上这个，否则会自动进入主进程
 
 if __name__ == '__main__':
     usage = "usage: %prog <bed> <output> <threads num> <email>"
@@ -136,5 +146,5 @@ if __name__ == '__main__':
     bed = pd.read_csv(options.bed_file_path, names=["chr", "start", "end", "GeneSymbol"], sep="\t")
     dir = options.dir  # 保存目录 
     print(f"saveing file to {dir}, please check")
-    downloadByThread(bed, email="xutingfeng@big.ac.cn", threads_num=options.threads_num, save_path=dir)
+    downloadByThread(bed, threads_num=options.threads_num, save_path=dir, email="xutingfeng@big.ac.cn")
     print(f"finish downloading.......")
